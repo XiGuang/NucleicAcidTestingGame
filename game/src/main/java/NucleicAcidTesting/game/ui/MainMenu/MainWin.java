718664e88b49f5d9ad246acd91adf63da0b59185
lzy <1016984801@qq.com>
2023-02-13 22:29:31 +0800

    修改排行榜样式

--- game/src/main/java/NucleicAcidTesting/game/ui/MainMenu/MainWin.java
+++ game/src/main/java/NucleicAcidTesting/game/ui/MainMenu/MainWin.java
@@ -17,6 +17,7 @@ import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import org.json.simple.JSONObject;
+
 import java.util.List;
 
 public class MainWin extends StackPane {
@@ -62,7 +63,8 @@ public class MainWin extends StackPane {
             Button setButton = new ChoiceButton("设置");
             setButton.setOnAction(actionEvent -> FXGL.getGameController().gotoGameMenu());
             Button rankButton = new ChoiceButton("排行榜");
-            rankButton.setOnAction(actionEvent -> {rankPane.toFront();
+            rankButton.setOnAction(actionEvent -> {
+                rankPane.toFront();
                 try {
                     getRank();
                 } catch (Exception e) {
@@ -71,16 +73,13 @@ public class MainWin extends StackPane {
             });
             Button exitButton = new ChoiceButton("退出");
             exitButton.setOnAction(actionEvent -> FXGL.getGameController().exit());
-            Button hellpButton = new ChoiceButton("帮助");
-            hellpButton.setOnAction(actionEvent -> new Alert(Alert.AlertType.INFORMATION, "TODO"));
 
-            this.getChildren().addAll(classicGameButton, infiniteGameButton, setButton, rankButton, exitButton, hellpButton);
+            this.getChildren().addAll(classicGameButton, infiniteGameButton, setButton, rankButton, exitButton);
             this.setPrefSize(boxWidth * 0.2, boxHeight);
             this.setStyle("-fx-background-color:rgba(153,204,255,0.6);");
             this.setAlignment(Pos.CENTER);
         }
     }
-
 
 
     class DetailPane extends Pane {
@@ -126,64 +125,82 @@ public class MainWin extends StackPane {
 
     private void setRankPane() {
         Image rankLogo = new Image("assets/textures/menuImg/rankLogo.png",
-                250, 100, false, false);
+                125, 50, false, false);
         ImageView rankLogoView = new ImageView(rankLogo);
-
         GridPane.setHalignment(rankLogoView, HPos.CENTER);
+        String emptyString = "                            ";
+        Text rankText = new Text("  排名");
+        Font font = Font.font("Times Roman", FontWeight.BOLD, 17);
+        rankText.setFont(font);
+        Text emptyText1 = new Text(emptyString);
+        emptyText1.setFont(font);
+        Text nameText = new Text("用户名");
+        nameText.setFont(font);
+        Text emptyText2 = new Text(emptyString);
+        emptyText2.setFont(font);
+        Text scoreText = new Text("得分");
+        scoreText.setFont(font);
+        HBox hBox = new HBox(rankText,emptyText1,nameText,emptyText2,scoreText);
+        hBox.setStyle("-fx-background-color:  #719cde;");
+        GridPane.setHalignment(hBox, HPos.CENTER);
+
+
         rankPane.add(rankLogoView, 0, 0);
-        rankPane.setVgap(20);
+        rankPane.add(hBox,0,1);
     }
 
     private void getRank() throws Exception {
         ScrollPane scrollPane = new ScrollPane();
         List<JSONObject> rankList = Client.Ranking();
         VBox vBox = new VBox();
-        vBox.setPrefSize(Config.WINDOW_WIDTH * 0.5,Config.WINDOW_HEIGHT);
+        vBox.setPrefSize(Config.WINDOW_WIDTH * 0.5, Config.WINDOW_HEIGHT*5);
 
         for (JSONObject jsonObject : rankList) {
             String ranking = jsonObject.get("Ranking").toString();
             Text rankingText = new Text(ranking);
             rankingText.setFont(Font.font("Times Roman", FontWeight.LIGHT, 20));
-            rankingText.resize(1000,1000);
-            String name =  jsonObject.get("Name").toString();
+
+            String name = jsonObject.get("Name").toString();
             Text nameText = new Text(name);
             nameText.setFont(Font.font("Times Roman", FontWeight.LIGHT, 20));
             String score = jsonObject.get("Number").toString();
             Text scoreText = new Text(score);
             scoreText.setFont(Font.font("Times Roman", FontWeight.LIGHT, 20));
             GridPane gridPane = new GridPane();
-            String gap1 = "----------------------------------";
-            String gap2 = "----------------------------------------";
+            String gap1 = "-------------------------------";
+            String gap2 = "---------------------------------------";
             String gap3 = "-----------";
             String empty = "     ";
             Text emptyText1 = new Text(empty);
             Text emptyText2 = new Text(empty);
             Text gapText1 = new Text(gap1);
             Text gapText2 = new Text(gap2);
-            Text gapText3= new Text(gap3);
-            gridPane.add(emptyText1,0,0);
-            gridPane.add(rankingText,1,0);
-            gridPane.add(nameText,2,0);
-            gridPane.add(scoreText,3,0);
-            gridPane.add(emptyText2,0,1);
-            gridPane.add(gapText1,1,1);
-            gridPane.add(gapText2,2,1);
-            gridPane.add(gapText3,3,1);
+            Text gapText3 = new Text(gap3);
+            gridPane.add(emptyText1, 0, 0);
+            gridPane.add(rankingText, 1, 0);
+            gridPane.add(nameText, 2, 0);
+            gridPane.add(scoreText, 3, 0);
+            gridPane.add(emptyText2, 0, 1);
+            gridPane.add(gapText1, 1, 1);
+            gridPane.add(gapText2, 2, 1);
+            gridPane.add(gapText3, 3, 1);
 
-            gridPane.setStyle("-fx-background-color: #99CCFF;");
+            gridPane.setStyle("-fx-background-color: transparent;");
             vBox.getChildren().add(gridPane);
 
         }
-        vBox.setStyle("-fx-background-color: #99CCFF;");
+        vBox.setStyle("-fx-background-color: #f5f5f5;");
         scrollPane.setContent(vBox);
-        scrollPane.setPrefSize(Config.WINDOW_WIDTH * 0.5,Config.WINDOW_HEIGHT*0.6);
+        scrollPane.setPrefSize(Config.WINDOW_WIDTH * 0.5, Config.WINDOW_HEIGHT * 0.7);
 
         scrollPane.setBackground(new Background(new BackgroundFill(Color.web("#99CCFF"), CornerRadii.EMPTY, Insets.EMPTY)));
-
+        scrollPane.setStyle("-fx-border-color: #2b2b2b;" +
+                "-fx-border-style: dotted;" +
+                "-fx-border-width: 2px;");
 
         scrollPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
         scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
-        rankPane.add(scrollPane, 0, 1);
+        rankPane.add(scrollPane, 0, 2);
 
 
     }
