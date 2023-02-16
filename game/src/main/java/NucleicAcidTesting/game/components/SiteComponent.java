0d5cfab60e7681592d4430443bc9d00e99368df7
wzx2001 <77987125+wzx2001@users.noreply.github.com>
2023-02-16 13:25:42 +0800

    实现了排队功能，排队队伍暂时还不能消失

--- game/src/main/java/NucleicAcidTesting/game/components/SiteComponent.java
+++ game/src/main/java/NucleicAcidTesting/game/components/SiteComponent.java
@@ -2,25 +2,75 @@ package NucleicAcidTesting.game.components;
 
 import NucleicAcidTesting.game.Config;
 import NucleicAcidTesting.game.NATType;
+import NucleicAcidTesting.game.tools.NATMath;
 import com.almasb.fxgl.core.math.FXGLMath;
 import com.almasb.fxgl.dsl.FXGL;
+import com.almasb.fxgl.dsl.components.KeepInBoundsComponent;
 import com.almasb.fxgl.entity.Entity;
 import com.almasb.fxgl.entity.SpawnData;
 import com.almasb.fxgl.entity.component.Component;
+import com.almasb.fxgl.entity.components.CollidableComponent;
 import com.almasb.fxgl.entity.components.ViewComponent;
+import com.almasb.fxgl.physics.PhysicsComponent;
+import com.almasb.fxgl.physics.box2d.dynamics.BodyType;
+import com.almasb.fxgl.time.LocalTimer;
 import javafx.geometry.Point2D;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.layout.StackPane;
+import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
+import javafx.util.Duration;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import static com.almasb.fxgl.dsl.FXGLForKtKt.getGameWorld;
 
 public class SiteComponent extends Component {
-    public void onAdded(){
+    private LocalTimer localTimer;
+    //检测队列
+    List<Entity>site_queue=new ArrayList<>();
+    final static double INFINITE_MAX=10000;
+    PhysicsComponent physicsComponent;
+    //距离
+    double distance=INFINITE_MAX;
 
+    public double getDistance(){
+        return distance;
+    }
+
+    //传入当前人的实体
+    public void onAdded(){
+        localTimer = FXGL.newLocalTimer();
+    }
+
+    public void onUpdate(double tpf) {
+        List<Entity> follow_list = FXGL.getWorldProperties().getObject("follow_list");
+        Entity player = follow_list.get(0);
+        distance=entity.distance(player);
+        if(distance<30) {
+            for (int i=1;i<follow_list.size();i++) {
+                Entity person = follow_list.get(i);
+                follow_list.remove(person);
+                FXGL.getGameWorld().removeEntity(person);
+                i--;
+                if(!site_queue.contains(person)) {
+                    site_queue.add(person);
+                    FXGL.getGameWorld().spawn("People",
+                            entity.getX() + Config.SPAWNING_X_GAP * site_queue.size(),
+                            entity.getBottomY() - 5);
+                }
+            }
+        }
+
+        //队列消失
+        while (!site_queue.isEmpty()) {
+            break;
+        }
     }
 }
+
+
