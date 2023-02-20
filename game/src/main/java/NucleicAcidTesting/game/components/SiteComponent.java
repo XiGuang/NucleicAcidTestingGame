d388b80ce58efc8024f7d3bdf86eb4eec5cdd427
wzx2001 <77987125+wzx2001@users.noreply.github.com>
2023-02-20 19:20:53 +0800

    新增加了打分表

--- game/src/main/java/NucleicAcidTesting/game/components/SiteComponent.java
+++ game/src/main/java/NucleicAcidTesting/game/components/SiteComponent.java
@@ -3,6 +3,7 @@ package NucleicAcidTesting.game.components;
 import NucleicAcidTesting.game.Config;
 import NucleicAcidTesting.game.NATType;
 import NucleicAcidTesting.game.tools.NATMath;
+import com.almasb.fxgl.core.collection.PropertyMap;
 import com.almasb.fxgl.core.math.FXGLMath;
 import com.almasb.fxgl.dsl.FXGL;
 import com.almasb.fxgl.dsl.components.KeepInBoundsComponent;
@@ -14,27 +15,34 @@ import com.almasb.fxgl.entity.components.ViewComponent;
 import com.almasb.fxgl.physics.PhysicsComponent;
 import com.almasb.fxgl.physics.box2d.dynamics.BodyType;
 import com.almasb.fxgl.time.LocalTimer;
+import javafx.beans.property.Property;
 import javafx.beans.property.SimpleDoubleProperty;
 import javafx.geometry.Point2D;
 import javafx.geometry.Rectangle2D;
+import javafx.scene.Node;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.util.Duration;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Properties;
 
 import static com.almasb.fxgl.dsl.FXGLForKtKt.getGameWorld;
 
 public class SiteComponent extends Component {
+    Text text;
     private int remove_count=0;
     private int mark=0;
+    int score=0;
     private LocalTimer localTimer;
     //检测队列
     List<Entity>site_queue=new ArrayList<>();
@@ -46,7 +54,18 @@ public class SiteComponent extends Component {
     }
 
     public void onAdded(){
+
         localTimer = FXGL.newLocalTimer();
+        Rectangle rectangle = new Rectangle(80,50);
+        rectangle.setStroke(Color.BROWN);
+        rectangle.setStrokeWidth(3);
+        text = FXGL.getUIFactoryService().newText(String.valueOf(score));
+        text.setFill(Color.BLUE);
+        text.fontProperty().unbind();
+        text.setFont(Font.font(35));
+
+        StackPane stackPane = new StackPane(rectangle,text);
+        FXGL.addUINode(stackPane,400,30);
     }
 
     public void onUpdate(double tpf) {
@@ -77,6 +96,7 @@ public class SiteComponent extends Component {
                     Entity citizen=FXGL.getGameWorld().spawn("People",
                             entity.getX()  + Config.SPAWNING_X_GAP * 4 * disappear_queue.size(),
                             entity.getBottomY() - 5);
+
                     disappear_queue.add(citizen);
                 }
             }
@@ -96,6 +116,7 @@ public class SiteComponent extends Component {
             FXGL.getGameWorld().removeEntity(person);
             disappear_queue.remove(0);
             mark++;
+            text.setText(String.valueOf(mark*10+score));
 
             for(int i=0;i<disappear_queue.size();i++){
                 disappear_queue.get(i).translate(-20,0);
