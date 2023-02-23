8b4925f3d7932a3a9ce8e1e613f8fa7c9a80b34d
selray <124846896+selray@users.noreply.github.com>
2023-02-23 19:10:11 +0800

    游戏界面增加了三个小按钮

--- game/src/main/java/NucleicAcidTesting/game/components/CountDownComponent.java
+++ game/src/main/java/NucleicAcidTesting/game/components/CountDownComponent.java
@@ -5,6 +5,7 @@ import NucleicAcidTesting.game.ui.LoadingWin;
 import com.almasb.fxgl.dsl.FXGL;
 import com.almasb.fxgl.entity.component.Component;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.ImagePattern;
@@ -70,6 +71,81 @@ public class CountDownComponent extends Component {
         //将背景图片与数字显示 结合
         StackPane stackPane = new StackPane(rectangle, text);
         FXGL.addUINode(stackPane, 850, 30);
+
+
+        //右下角退出按钮
+        Image exitButton_up = new Image("assets/textures/CountDownPic/exit_down(已去底).png");
+        Image exitButton_down = new Image("assets/textures/CountDownPic/exit_up(已去底).png");
+        ImageView exitButton = new ImageView(exitButton_up);
+        exitButton.setFitWidth(50);
+        exitButton.setFitHeight(50);
+
+        exitButton.setOnMouseClicked(event -> {
+
+            FXGL.getGameController().gotoMainMenu();
+            //this.setVisible(false);
+        });
+        exitButton.setOnMouseEntered(event -> exitButton.setImage(exitButton_down));
+        exitButton.setOnMouseExited(event -> exitButton.setImage(exitButton_up));
+
+        FXGL.addUINode(exitButton, 900, 450);
+
+        //右下角暂停/继续按钮
+        Image pauseButton_up = new Image("assets/textures/CountDownPic/pause1(已去底).png");
+        Image pauseButton_down = new Image("assets/textures/CountDownPic/pause1_down(已去底).png");
+        ImageView pauseButton = new ImageView(pauseButton_up);
+        pauseButton.setFitWidth(50);
+        pauseButton.setFitHeight(50);
+
+        Image continueButton_up = new Image("assets/textures/CountDownPic/continue1(已去底).png");
+        Image continueButton_down = new Image("assets/textures/CountDownPic/continue1_down(已去底).png");
+        ImageView continueButton = new ImageView(continueButton_up);
+        continueButton.setFitWidth(50);
+        continueButton.setFitHeight(50);
+        pauseButton.setVisible(true);
+        continueButton.setVisible(false);
+
+        pauseButton.setOnMouseEntered(event -> pauseButton.setImage(pauseButton_down));
+        pauseButton.setOnMouseExited(event -> pauseButton.setImage(pauseButton_up));
+
+        continueButton.setOnMouseEntered(event -> continueButton.setImage(continueButton_down));
+        continueButton.setOnMouseExited(event -> continueButton.setImage(continueButton_up));
+
+        pauseButton.setOnMouseClicked(event -> {
+                //暂停游戏
+                FXGL.getGameController().pauseEngine();
+                //改变图标
+                pauseButton.setVisible(false);
+                continueButton.setVisible(true);
+                //this.setVisible(false);
+        });
+        continueButton.setOnMouseClicked(event -> {
+            //暂停游戏
+            FXGL.getGameController().resumeEngine();
+            //改变图标
+            pauseButton.setVisible(true);
+            continueButton.setVisible(false);
+            //this.setVisible(false);
+        });
+        FXGL.addUINode(pauseButton, 780, 450);
+        FXGL.addUINode(continueButton, 780, 450);
+
+        //重置按钮
+        Image resetButton_up = new Image("assets/textures/CountDownPic/resetButton_up(已去底).png");
+        Image resetButton_down = new Image("assets/textures/CountDownPic/resetButton_down(已去底).png");
+        ImageView ResetButton = new ImageView(resetButton_up);
+        ResetButton.setFitWidth(50);
+        ResetButton.setFitHeight(50);
+
+
+        ResetButton.setOnMouseClicked(event -> {
+            FXGL.getGameController().resumeEngine();
+            FXGL.getGameController().startNewGame();
+            //this.setVisible(false);
+        });
+        ResetButton.setOnMouseEntered(event -> ResetButton.setImage(resetButton_down));
+        ResetButton.setOnMouseExited(event -> ResetButton.setImage(resetButton_up));
+        FXGL.addUINode(ResetButton,840,450);
     }
 
     @Override
